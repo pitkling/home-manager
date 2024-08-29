c0a4575e576fa965a49ccd7b1f6d0a8994f7dff0
Peter Kling <peter.kling@uni-hamburg.de>
2024-08-29 21:07:07 +0200

    refactor launchd.agents.syncthing
    Make launchd.agents an explicit attribute set, since we're going to add another agent.

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -679,15 +679,17 @@ in {
         };
       };
 
-      launchd.agents.syncthing = {
-        enable = true;
-        config = {
-          ProgramArguments = syncthingArgs;
-          KeepAlive = {
-            Crashed = true;
-            SuccessfulExit = false;
+      launchd.agents = {
+        syncthing = {
+          enable = true;
+          config = {
+            ProgramArguments = syncthingArgs;
+            KeepAlive = {
+              Crashed = true;
+              SuccessfulExit = false;
+            };
+            ProcessType = "Background";
           };
-          ProcessType = "Background";
         };
       };
     })
